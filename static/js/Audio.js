8adfceff72cc8a978db153ee066ec5a65ad7a263
tori2 <ax4xtq3dq25b@gmail.com>
2017-12-27 17:26:21 +0900

    add class Audio

---
+++ static/js/Audio.js
@@ -0,0 +1,44 @@
+function basename(path){
+  return path.split('/').pop().split('.')[0]
+}
+
+class Audio {
+  constructor(){
+    this.context = null
+    this.buffer = {}
+  }
+  init(){
+    window.AudioContext = window.AudioContext||window.webkitAudioContext
+    this.context = new AudioContext()
+  }
+  loadSound({key, url}) {
+    var request = new XMLHttpRequest()
+    request.open('GET', url, true)
+    request.responseType = 'arraybuffer'
+
+    const context = this.context
+    const buffer = this.buffer
+  
+    // Decode asynchronously
+    request.onload = function() {
+      context.decodeAudioData(request.response, function(buf) {
+        var _key = key || basename(url)
+        buffer[_key] = buf
+      }, (err) => {
+        console.error(e)
+      })
+    }
+    request.send()
+  }
+  playSound(buffer) {
+    var source = this.context.createBufferSource() // creates a sound source
+    source.buffer = buffer                    // tell the source which sound to play
+    source.connect(this.context.destination)       // connect the source to the context's destination (the speakers)
+    source.start(0)                           // play the source now
+  }
+  play(key){
+    this.playSound(this.buffer[key])
+  }
+  
+}
+export default Audio
--- static/js/sound.js
+++ static/js/sound.js
@@ -1,61 +1,26 @@
-var context
-var bufferList = []
+const audio = new Audio()
 
 window.addEventListener('load', init, false)
 
 function init() {
   try {
-    window.AudioContext = window.AudioContext||window.webkitAudioContext
-    context = new AudioContext()
+    audio.init()
   }
   catch(e) {
     alert('Web Audio API is not supported in this browser')
     return
   }
-  load()
-}
-
-function load(){
-  try {
-    // 非同期でロードされるため明示的に格納インデックスを指定してロードする
-    loadSound(0, './static/sound/base.mp3')
-    loadSound(1, './static/sound/se_maoudamashii_onepoint22.mp3')
-  }
-  catch(e) {
-    console.error(e)
-  }
-}
-
-function loadSound(index, url) {
-  var request = new XMLHttpRequest()
-  request.open('GET', url, true)
-  request.responseType = 'arraybuffer'
-
-  // Decode asynchronously
-  request.onload = function() {
-    context.decodeAudioData(request.response, function(buffer) {
-      bufferList[index] = buffer
-    }, (err) => {
-      console.error(e)
-    })
-  }
-  request.send()
-}
-
-function playSound(buffer) {
-  var source = context.createBufferSource() // creates a sound source
-  source.buffer = buffer                    // tell the source which sound to play
-  source.connect(context.destination)       // connect the source to the context's destination (the speakers)
-  source.start(0)                           // play the source now
+  audio.loadSound({url: './static/base.mp3'})
+  audio.loadSound({key:'pirori', url: './static/se_maoudamashii_onepoint22.mp3'})
 }
 
 document.addEventListener('keydown', keyDown)
 
 function play1() {
-  playSound(bufferList[0])
+  audio.play('base')
 }  
 function play2() {
-  playSound(bufferList[1])
+  audio.play('pirori')
 }  
 
 function keyDown(e) {
