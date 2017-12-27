67f19163bee5de99090f0a1071d5f6e4afc03cc7
tori2 <ax4xtq3dq25b@gmail.com>
2017-12-27 12:38:30 +0900

    整理

--- static/js/sound.js
+++ static/js/sound.js
@@ -1,51 +1,21 @@
-console.log('*sound.js')
+var context
+var bufferList = []
 
-var context;
-var dogBarkingBuffer = null;
-var bufferList = [];
-
-window.addEventListener('load', init, false);
+window.addEventListener('load', init, false)
 
 function init() {
   try {
-    // Fix up for prefixing
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
-    context = new AudioContext();
-
-console.log('*init AudioContext OK')
-
+    window.AudioContext = window.AudioContext||window.webkitAudioContext
+    context = new AudioContext()
   }
   catch(e) {
-    alert('Web Audio API is not supported in this browser');
+    alert('Web Audio API is not supported in this browser')
+    return
   }
-
-  decode()
+  load()
 }
 
-function loadSound(url, cb) {
-console.log('*loadSound')
-
-  var request = new XMLHttpRequest();
-  request.open('GET', url, true);
-  request.responseType = 'arraybuffer';
-
-  // Decode asynchronously
-  request.onload = function() {
-    // cb(request.response)
-    context.decodeAudioData(request.response, function(buffer) {
-      // dogBarkingBuffer = buffer;
-      bufferList[bufferList.length] = buffer;
-    }, (err) => {
-      console.error(e);
-    });
-
-  }
-  request.send();
-}
-
-
-function decode(){
-console.log('*decode')
+function load(){
   try {
     loadSound('./static/sound/base.mp3')
     loadSound('./static/sound/se_maoudamashii_onepoint22.mp3')
@@ -55,11 +25,28 @@ console.log('*decode')
   }
 }
 
+function loadSound(url, cb) {
+  var request = new XMLHttpRequest()
+  request.open('GET', url, true)
+  request.responseType = 'arraybuffer'
+
+  // Decode asynchronously
+  request.onload = function() {
+    context.decodeAudioData(request.response, function(buffer) {
+      bufferList[bufferList.length] = buffer
+    }, (err) => {
+      console.error(e)
+    })
+  }
+  request.send()
+}
+
+
 function playSound(buffer) {
-  var source = context.createBufferSource(); // creates a sound source
-  source.buffer = buffer;                    // tell the source which sound to play
-  source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-  source.start(0);                           // play the source now
+  var source = context.createBufferSource() // creates a sound source
+  source.buffer = buffer                    // tell the source which sound to play
+  source.connect(context.destination)       // connect the source to the context's destination (the speakers)
+  source.start(0)                           // play the source now
 }
 
 function play1() {
@@ -69,16 +56,9 @@ function play2() {
   playSound(bufferList[1])
 }  
 
-
-document.addEventListener('keydown', keyDown);
+document.addEventListener('keydown', keyDown)
 
 function keyDown(e) {
-  console.log('keydown', e.key)
-
-  if(e.key === 'a') {
-    play1()
-  }
-  if(e.key === 's') {
-    play2()
-  }
+  if(e.key === 'a') play1()
+  if(e.key === 's') play2()
 }
